e4d71061eef15aaa989587db2206ef113a85af64
“alex” <poryvvotryv@gmail.com>
2024-02-05 18:34:17 +0200

    Fix some logic bugs

--- src/index.js
+++ src/index.js
@@ -3,31 +3,44 @@
 const fathersYearsOld = prompt("How old is Fathrer?", 28);
 const sonsYearsOld = prompt("How old is Son?", 20);
 
-const ageCount = (father, son) => {
-    let res = 0;
- 
-    if (father <= 0 ||
-        son <= 0) {
-        alert("Extra younger person!")
-        location.reload()
-    }
-    
-    if( (father - son) > 15 ||
-        (son - father) > 15){
-        alert("Extra big age difference!")
-        location.reload()
+const ageCount = (father, son) => { 
+    if (son <= 0) {
+        alert("A son hasn't been born yet at this age!")
+        window.location.reload();
+        return false;
     }
 
+    if (father < 16) {
+        alert("Extra young father! He should be 16 y. o. at least!")
+        window.location.reload();
+        return false;
+    }
+
+    if( (father - son) > 15 ||
+        (son - father) > 15 ){
+        alert("Extra big age difference!")
+        window.location.reload()
+        return false;
+    }
+
+    if ((father - son < 0 ||
+         father == son)) {
+        alert("So strange family!")
+        window.location.reload()
+        return false;
+    }
+
+    let res = 0;
     let neededAgeSon = father - son;
     let neededAgeFather = (neededAgeSon * 2);
     
     if(neededAgeFather > father) {
         res = neededAgeFather - father
+        alert(`Father will be twofold older than son in ${res} years`)
     } else {neededAgeFather < father} {
         res = father - neededAgeFather;
+        alert(`Father was twofold older than son ${res} years ago`)
     }
-    
-    return res;
 }
 
-console.log(ageCount(fathersYearsOld, sonsYearsOld))
+ageCount(fathersYearsOld, sonsYearsOld)
