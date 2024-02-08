e48c7f99c6dd4afaa6aa1800d10918ae58f63b1f
“alex” <poryvvotryv@gmail.com>
2024-02-08 13:28:00 +0200

    Fix if clause

--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
 "use strict"
 
-const fathersYearsOld = prompt("How old is Fathrer?", 28);
-const sonsYearsOld = prompt("How old is Son?", 20);
+// const fathersYearsOld = prompt("How old is Fathrer?", 28);
+// const sonsYearsOld = prompt("How old is Son?", 20);
 
 /** Documentation of ageCount
  * @param {number} father 
  * @param {number} son 
- */
+ * @return {string} - when father twofold older than son.
+*/  
 
 const ageCount = (father, son) => { 
     if (son <= 0) {
-        alert("A son hasn't been born yet at this age!")
-        window.location.reload();
-        return false;
+        return "A son hasn't been born yet at this age!";
     }
 
-    if (father < 16) {
-        alert("Extra young father! He should be 16 y. o. at least!")
-        window.location.reload();
-        return false;
-    }
-
-    if( (father - son) > 15 ||
-        (son - father) > 15 ){
-        alert("Extra big age difference!")
-        window.location.reload()
-        return false;
-    }
-
-    if ((father - son < 0 ||
-         father == son)) {
-        alert("So strange family!")
-        window.location.reload()
-        return false;
+    if( father - son <= 15){
+        return "Extra small age difference!";
     }
 
     let res = 0;
@@ -40,12 +23,17 @@ const ageCount = (father, son) => {
     let neededAgeFather = (neededAgeSon * 2);
     
     if(neededAgeFather > father) {
-        res = neededAgeFather - father
-        alert(`Father will be twofold older than son in ${res} years`)
-    } else {neededAgeFather < father} {
+        res = neededAgeFather - father;
+        return `Father will be twofold older than son in ${res} years`;
+    } else if (neededAgeFather < father) {
         res = father - neededAgeFather;
-        alert(`Father was twofold older than son ${res} years ago`)
+        return `Father was twofold older than son ${res} years ago`;
+    } else if (neededAgeFather === father) {
+        return 'Father is twofold older than son this year';
     }
 }
 
-ageCount(fathersYearsOld, sonsYearsOld)
+console.log(ageCount(28, 2))
+console.log(ageCount(25, 0))
+console.log(ageCount(20, 10))
+console.log(ageCount(24, 8))
